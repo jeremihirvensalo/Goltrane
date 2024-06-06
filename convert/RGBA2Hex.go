20ec3cd0706a0b43ca9598df37827891c15cfe07
JeremiH04 <jeremi.hirvensalo@gmail.com>
2024-06-06 15:59:04 +0300

    Removed commented out code

--- convert/RGBA2Hex.go
+++ convert/RGBA2Hex.go
@@ -11,10 +11,7 @@ func transformAlpha(alpha string) string {
 	alphaVal, err := strconv.ParseFloat(alpha, 64)
 	internal.CheckErr(err)
 
-	// val := math.RoundToEven(alphaVal * 255)
-	// val := alphaVal * 255
 	return fmt.Sprintf("%d", int(alphaVal*255))
-
 }
 
 func RGBA2Hex(data string) string {
@@ -24,15 +21,6 @@ func RGBA2Hex(data string) string {
 
 	for i := 0; i < len(cleanVals); i++ {
 		numbers := strings.Split(cleanVals[i], ",")
-
-		// alpha := numbers[3]
-
-		// numbers = numbers[:3]
-
-		// alpha, err := strconv.ParseFloat(numbers[3], 64)
-		// internal.CheckErr(err)
-
-		// alphaVal := int64(alpha * 255)
 
 		numbers[3] = transformAlpha(numbers[3])
 
