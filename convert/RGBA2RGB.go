c0a4484d59b4b5c87e7b377a0efd720cce2a8c65
Jeremi Hirvensalo <jeremi.hirvensalo@karhuhelsinki.fi>
2024-06-07 09:35:12 +0300

    RGBA2RGB conversion

--- convert/RGBA2RGB.go
+++ convert/RGBA2RGB.go
@@ -1,7 +1,9 @@
 package convert
 
 import (
+	"fmt"
 	"karhuhelsinki/goltrane/internal"
+	"math"
 	"strings"
 )
 
@@ -13,6 +15,20 @@ func RGBA2RGB(data string) string {
 		numbers := strings.Split(cleanVals[i], ",")
 
 		numbers[3] = internal.TransformAlpha(numbers[3])
+
+		normRed := float64(internal.ToInt(numbers[0])) / 255.0
+		normGreen := float64(internal.ToInt(numbers[1])) / 255.0
+		normBlue := float64(internal.ToInt(numbers[2])) / 255.0
+
+		alph := float64(internal.ToInt(numbers[3])) / 255.0
+
+		red := int(math.Round(alph * normRed * 255.0))
+		green := int(math.Round(alph * normGreen * 255.0))
+		blue := int(math.Round(alph * normBlue * 255.0))
+
+		rgb := fmt.Sprintf("rgb(%d, %d, %d)", red, green, blue)
+
+		data = strings.Replace(data, hits[i], rgb, -1)
 	}
 
 	return data
