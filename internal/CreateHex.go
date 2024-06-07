1506745568b61f997ef1d4d9f6e7ed8dc7bec35b
Jeremi Hirvensalo <jeremi.hirvensalo@karhuhelsinki.fi>
2024-06-07 09:09:00 +0300

    Fix some HEX values omitting "0"

--- internal/CreateHex.go
+++ internal/CreateHex.go
@@ -6,7 +6,7 @@ func CreateHex(numbers []string) string {
 	hex := "#"
 
 	for i := 0; i < len(numbers); i++ {
-		hex += fmt.Sprintf("%X", ToInt(numbers[i]))
+		hex += fmt.Sprintf("%02X", ToInt(numbers[i]))
 	}
 
 	return hex
